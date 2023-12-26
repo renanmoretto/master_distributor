98b5eed436a1713f87273a790af37f492df1db84
renanmoretto <himynameisrenan@outlook.com>
2023-12-26 16:30:47 -0300

    add

---
+++ master_distributor/_core.py
@@ -0,0 +1,187 @@
+import random
+from typing import TypedDict, Callable, Any
+
+import pandas as pd
+import polars as pl
+
+
+class TradeID(TypedDict):
+    BROKER: str
+    TICKER: str
+    SIDE: str
+
+
+class Distribution(TypedDict):
+    BROKER: str
+    TICKER: str
+    SIDE: str
+    QUANTITY: int
+    PRICE: float
+    PORTFOLIO: str
+
+
+def _ensure_columns(
+    df: pd.DataFrame,
+    required_columns: list[str],
+    int_columns: list[str],
+    float_columns: list[str],
+) -> pd.DataFrame:
+    missing_cols = [col for col in required_columns if col not in df.columns]
+    if missing_cols:
+        raise ValueError(f'missing columns on dataframe {missing_cols}')
+
+    df = df.copy()
+
+    for int_col in int_columns:
+        df[int_col] = df[int_col].astype(int)  # type: ignore
+
+    for float_col in float_columns:
+        df[float_col] = df[float_col].astype(float)  # type: ignore
+
+    return df
+
+
+def _parse_trades_master(trades_master: pd.DataFrame) -> pl.LazyFrame:
+    df = trades_master.copy()
+    df = _ensure_columns(
+        df=df,
+        required_columns=['BROKER', 'TICKER', 'SIDE', 'QUANTITY', 'PRICE'],
+        int_columns=['QUANTITY'],
+        float_columns=['PRICE'],
+    )
+    return pl.from_pandas(df).lazy()
+
+
+def _parse_allocations(allocations: pd.DataFrame) -> pl.LazyFrame:
+    df = allocations.copy()
+    df = _ensure_columns(
+        df=df,
+        required_columns=['BROKER', 'TICKER', 'SIDE', 'QUANTITY', 'PORTFOLIO'],
+        int_columns=['QUANTITY'],
+        float_columns=[],
+    )
+    return pl.from_pandas(df).lazy()
+
+
+def _compare_quantitites(master: pl.LazyFrame, allocations: pl.LazyFrame):
+    master_gpd = master.group_by(['BROKER', 'TICKER', 'SIDE']).sum().drop(['PRICE'])
+    allocations_gpd = (
+        allocations.group_by(['BROKER', 'TICKER', 'SIDE']).sum().drop(['PORTFOLIO'])
+    )
+    if not master_gpd.collect().equals(allocations_gpd.collect()):
+        raise ValueError('Quantities for master and allocations are not equal')
+
+
+def _filter_lazy_by_trade_id(
+    lazyframe: pl.LazyFrame,
+    trade_id: TradeID,
+) -> pl.LazyFrame:
+    return lazyframe.filter(
+        (pl.col('BROKER') == trade_id['BROKER'])
+        & (pl.col('TICKER') == trade_id['TICKER'])
+        & (pl.col('SIDE') == trade_id['SIDE'])
+    )
+
+
+def _distribute_trade_id(
+    master_trade_id: list[tuple[str, str, str, int, float]],
+    allocations_trade_id: list[tuple[str, str, str, int, str]],
+    qty_calculator: Callable[[dict[str, Any]], int],
+    shuffle_orders: bool = True,
+) -> list[Distribution]:
+    orders = list(master_trade_id)
+    if shuffle_orders:
+        random.shuffle(orders)
+
+    _qts = (tup[3] for tup in allocations_trade_id)
+    _portfolios = (tup[4] for tup in allocations_trade_id)
+    vertical_qty_per_portfolio = {
+        portfolio: qty for portfolio, qty in zip(_portfolios, _qts) if qty != 0
+    }
+    remaining_vertical_qty_per_portfolio = vertical_qty_per_portfolio
+
+    portfolios = tuple(remaining_vertical_qty_per_portfolio.keys())
+
+    distribution_trade_id: list[Distribution] = []
+
+    for order in orders:
+        broker = order[0]
+        ticker = order[1]
+        side = order[2]
+        quantity = order[3]
+        price = order[4]
+
+        remaining_order_qty = quantity
+
+        while remaining_order_qty > 0:
+            for portfolio in portfolios:
+                if remaining_order_qty == 0:
+                    break
+                max_qty_portfolio = remaining_vertical_qty_per_portfolio[portfolio]
+                if max_qty_portfolio == 0:
+                    continue
+
+                qty = qty_calculator(locals())
+
+                remaining_order_qty -= qty
+                remaining_vertical_qty_per_portfolio[portfolio] -= qty
+
+                distribution_trade_id.append(
+                    Distribution(
+                        BROKER=broker,
+                        TICKER=ticker,
+                        SIDE=side,
+                        QUANTITY=qty,
+                        PRICE=price,
+                        PORTFOLIO=portfolio,
+                    )
+                )
+    return distribution_trade_id
+
+
+def _distribute_trade_ids(
+    master_lazy: pl.LazyFrame,
+    allocations_lazy: pl.LazyFrame,
+    trade_ids: list[TradeID],
+    qty_calculator: Callable[[dict[str, Any]], int],
+    shuffle_orders: bool = True,
+) -> list[Distribution]:
+    distributions: list[Distribution] = []
+    for trade_id in trade_ids:
+        master_trade_id = _filter_lazy_by_trade_id(master_lazy, trade_id)
+        allocations_trade_id = _filter_lazy_by_trade_id(allocations_lazy, trade_id)
+
+        trade_id_distribution = _distribute_trade_id(
+            master_trade_id=master_trade_id.collect().rows(),  # type: ignore
+            allocations_trade_id=allocations_trade_id.collect().rows(),  # type: ignore
+            qty_calculator=qty_calculator,
+            shuffle_orders=shuffle_orders,
+        )  # type: ignore
+
+        distributions += trade_id_distribution
+    return distributions
+
+
+def default_distribute(
+    trades_master: pd.DataFrame,
+    allocations: pd.DataFrame,
+    qty_calculator: Callable[[dict[str, Any]], int],
+    shuffle_orders: bool = True,
+) -> pd.DataFrame:
+    master_lazy: pl.LazyFrame = _parse_trades_master(trades_master)
+    allocations_lazy: pl.LazyFrame = _parse_allocations(allocations)
+    _compare_quantitites(master_lazy, allocations_lazy)
+
+    trade_ids: list[TradeID] = (
+        master_lazy.select(['BROKER', 'TICKER', 'SIDE']).collect().unique().to_dicts()
+    )  # type: ignore
+
+    distribution = _distribute_trade_ids(
+        master_lazy=master_lazy,
+        allocations_lazy=allocations_lazy,
+        trade_ids=trade_ids,
+        qty_calculator=qty_calculator,
+        shuffle_orders=shuffle_orders,
+    )
+
+    return pd.DataFrame(distribution)
--- master_distributor/distributors.py
+++ master_distributor/distributors.py
@@ -1,7 +1,65 @@
-from enum import Enum, auto
+import random
+
+from abc import ABC, abstractmethod
+from typing import Any, Optional
+
+import pandas as pd
+
+from ._core import default_distribute
 
 
-class Distributors(Enum):
-    RandomDistributor = auto()
-    UnitDistributor = auto()
-    BestDistributor = auto()
+Kwargs = dict[str, Any]
+
+
+class Distributor(ABC):
+    @staticmethod
+    @abstractmethod
+    def _qty_calculator(kw_locals: dict[str, Any]) -> int:
+        pass
+
+    @abstractmethod
+    def distribute(
+        self, trades_master: pd.DataFrame, allocations: pd.DataFrame
+    ) -> pd.DataFrame:
+        pass
+
+
+class RandomDistributor(Distributor):
+    def __init__(
+        self,
+        shuffle_orders: bool = False,
+        loop: bool = False,
+        std_break: Optional[float] = None,
+        else_return_best: bool = True,
+        max_its: int = 50_000,
+        verbose: bool = False,
+    ):
+        self._shuffle_orders = shuffle_orders
+        self._loop = loop
+
+    @staticmethod
+    def _qty_calculator(kw_locals: dict[str, Any]) -> int:
+        remaining_order_qty = int(kw_locals.get('remaining_order_qty'))  # type: ignore
+        max_qty_portfolio = int(kw_locals.get('max_qty_portfolio'))  # type: ignore
+        max_qty_random = min(max_qty_portfolio, remaining_order_qty)
+        return random.randint(1, max_qty_random)
+
+    def distribute(
+        self,
+        trades_master: pd.DataFrame,
+        allocations: pd.DataFrame,
+    ) -> pd.DataFrame:
+        return default_distribute(
+            trades_master=trades_master,
+            allocations=allocations,
+            qty_calculator=self._qty_calculator,
+            shuffle_orders=self._shuffle_orders,
+        )
+
+
+class UnitDistributor(Distributor):
+    pass
+
+
+class BestDistributor(Distributor):
+    pass
