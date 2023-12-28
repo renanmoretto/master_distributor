bc01b0b2cf38f1c555b4c0186816e2cee81d645c
renanmoretto <himynameisrenan@outlook.com>
2023-12-28 16:22:00 -0300

    format

--- master_distributor/distributors/_types.py
+++ master_distributor/distributors/_types.py
@@ -6,8 +6,8 @@ TupleAllocationAlias = tuple[str, int]
 
 TupleDistributionAlias = tuple[int, float, str]
 
+TupleFullDistributionAlias = tuple[str, str, str, int, float, str]
+
 FuncDistributeAlias = Callable[
     [list[TupleTradesAlias], list[TupleAllocationAlias]], list[TupleDistributionAlias]
 ]
-
-TupleFullDistributionAlias = tuple[str, str, str, int, float, str]
