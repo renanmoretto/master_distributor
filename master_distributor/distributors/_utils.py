eb316c72fd11e094a90fa59c5a31f5aea4faaadc
renanmoretto <himynameisrenan@outlook.com>
2023-12-28 15:03:18 -0300

    added dist avg price func

--- master_distributor/distributors/_utils.py
+++ master_distributor/distributors/_utils.py
@@ -1,19 +1,33 @@
-# TODO
+from collections import defaultdict
+from typing import Union
+
 import pandas as pd
 import polars as pl
 
 from ._slice_distributors import TupleDistributionAlias
 
 
-def distribution_average_price(
+def _distribution_average_price(
     distribution: list[TupleDistributionAlias],
 ) -> dict[str, float]:
     """Returns a dict containing the average price for each portfolio"""
-    ...
+    portfolio_totals: dict[str, dict[str, Union[float, int]]] = defaultdict(
+        dict[str, Union[float, int]]
+    )
+    for qty, price, portfolio in distribution:
+        portfolio_totals[portfolio]['QUANTITY'] += qty
+        portfolio_totals[portfolio]['VOLUME'] += qty * price
+
+    average_price_per_portfolio: dict[str, float] = defaultdict(float)
+    for _, _, portfolio in distribution:
+        total_volume = portfolio_totals[portfolio]['VOLUME']
+        total_qty = portfolio_totals[portfolio]['QUANTITY']
+        average_price_per_portfolio[portfolio] = total_volume / total_qty
+    return average_price_per_portfolio
 
 
 def distribution_max_deviation(distribution: list[TupleDistributionAlias]) -> float:
-    _dist_average_price = distribution_average_price(distribution)
+    _dist_average_price = _distribution_average_price(distribution)
 
     # Remove after impl
     return 1.0
